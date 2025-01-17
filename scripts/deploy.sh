b2c11e8759e845674e92c4dbd43ac769c5ca61ed
Renjie Deng <dengrenjie31@hotmail.com>
2025-01-17 10:18:20 +0800

    fix: deploy.sh
    Signed-off-by: Renjie Deng <dengrenjie31@hotmail.com>

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -4,4 +4,4 @@ docker stop subconverter
 docker rm subconverter
 docker rmi dengrenjie31/subconverter
 docker pull dengrenjie31/subconverter
-docker-compose up -d --remove-orphans
+docker compose up -d --remove-orphans
